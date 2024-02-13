0d2f06d7db3c8c3f755fe9567b0d95df286dfb42
asfirodi <asfirodi@ncsu.edu>
2024-02-12 19:44:49 -0500

    Contact and Header component initialized

---
+++ contactmanager/src/components/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+class Contact extends Component {
+  render() {
+    return (
+      <div>
+        <h4>Abhi</h4>
+        <ul>
+          <li>Email: abhi@example.com</li>
+          <li>Phone: 9876543210</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default Contact;
---
+++ contactmanager/src/components/Header.js
@@ -0,0 +1,12 @@
+// No State needed. Just want to show static Markup through JSX
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div>
+      <h1>Contact Manager</h1>
+    </div>
+  )
+}
+
+export default Header;
