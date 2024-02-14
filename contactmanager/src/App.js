944936f524d069e3aaa45ef448f7092234281d1f
asfirodi <asfirodi@ncsu.edu>
2024-02-13 19:23:14 -0500

    props, defaultProps, propTypes, JSX, Components

--- contactmanager/src/App.js
+++ contactmanager/src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Contact from './components/Contact';
 import Header from './components/Header';
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header branding="Contact Manager" />
-        <Contact naame="Abby" email="abby@example.com" phone="9876543210"/>
-        <Contact naame="Bobby" email="bobby@example.com" phone="9786543120"/>
+        <div className="container">
+          <Contact name="Abby" email="abby@example.com" phone="9876543210"/>
+          <Contact name="Bobby" email="bobby@example.com" phone="9786543120"/>
+        </div>
       </div>
     );
   }
--- contactmanager/src/components/Contact.js
+++ contactmanager/src/components/Contact.js
@@ -1,18 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+// import './contact.css'; // external css style sheet, overrides everything
 
 class Contact extends Component {
+  // Expected prop type, gives warning if given wrong prop type / Type validation
+  // inside Class
+  static propTypes = {
+    name: PropTypes.string.isRequired,
+    email: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired
+  };
+
   render(props) {
     const { email, phone } = this.props; // destructuring
     return (
-      <div>
+      <div className="card card-body mb-3">
         <h4>{this.props.name}</h4> {/* direct use with this.props */}
-        <ul>
-          <li>Email: {email}</li>
-          <li>Phone: {phone}</li>
+        <ul className='list-group'>
+          <li className="list-group-item">Email: {email}</li>
+          <li className="list-group-item">Phone: {phone}</li>
         </ul>
       </div>
     )
   }
 }
+
+// Expected prop type, gives warning if given wrong prop type / Type validation
+// outside Class
+// Contact.propTypes = {
+//   name: PropTypes.string.isRequired,
+//   email: PropTypes.string.isRequired,
+//   phone: PropTypes.string.isRequired
+// };
 
 export default Contact;
